8d5528616d9e754a60f29c37aadaf3b8e9386644
Felix Zedén Yverås <felix@fzy.se>
2024-12-02 11:11:09 +0100

    test(2024/02): add test to ensure part 1 answer does not change
    Part 2 will require some  modifications to the Report struct. Adding a
    test for part 1 makes sure these modifications do not affect the answer
    of part 1.

--- solutions/2024/day02/rust/src/part1.rs
+++ solutions/2024/day02/rust/src/part1.rs
@@ -1,9 +1,21 @@
 use crate::report::Report;
 
 pub fn part1_main() {
+    println!("Safe reports: {}", get_safe_report_count());
+}
+
+fn get_safe_report_count() -> usize {
     let input = include_str!("input.txt");
     let reports: Vec<Report> = input.lines().map(|line| Report::parse(line)).collect();
-    let safe_count = reports.iter().filter(|r| r.is_safe()).count();
+    reports.iter().filter(|r| r.is_safe()).count()
+}
 
-    println!("Safe reports: {}", safe_count);
+#[cfg(test)]
+mod tests {
+    use crate::part1::get_safe_report_count;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(get_safe_report_count(), 639);
+    }
 }
