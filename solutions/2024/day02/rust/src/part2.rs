799e8f7affab6ee6e35ac90a9275bd1b7b020585
Felix Zedén Yverås <felix@fzy.se>
2024-12-02 11:20:03 +0100

    test(2024/02): add test to ensure part 2 answer does not change

--- solutions/2024/day02/rust/src/part2.rs
+++ solutions/2024/day02/rust/src/part2.rs
@@ -9,3 +9,13 @@ fn get_safe_report_count() -> usize {
     let reports: Vec<Report> = input.lines().map(|line| Report::parse(line)).collect();
     reports.iter().filter(|r| r.is_safe(1)).count()
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::part2::get_safe_report_count;
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(get_safe_report_count(), 674);
+    }
+}
