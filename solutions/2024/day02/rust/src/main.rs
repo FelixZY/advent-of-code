c0566a516edee5b4036606bb7108aa17fd73e1b8
Felix Zedén Yverås <felix@fzy.se>
2024-12-02 11:19:27 +0100

    feat(2024/02): complete part 2
    My intuition tells me this is not a correct solution, even though AoC
    accepted it. Specifically:
    * I get the sense that flipping the `is_increasing` bool each time the
      report starts growing the opposite direction may cause a miscount.
    * If a level causes _both_ a direction change and an out-of-bounds error
      (as in the theoretical report `1 2 -9 3`), that's actually two
      discrepancies according to my program, even though removing a single
      bad level would fix the issue.

--- solutions/2024/day02/rust/src/main.rs
+++ solutions/2024/day02/rust/src/main.rs
@@ -1,9 +1,14 @@
 use crate::part1::part1_main;
+use crate::part2::part2_main;
 
 mod part1;
 mod report;
+mod part2;
 
 fn main() {
     println!("Part 1");
     part1_main();
+
+    println!("Part 2");
+    part2_main();
 }
