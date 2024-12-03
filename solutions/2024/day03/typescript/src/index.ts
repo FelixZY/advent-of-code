8cffbfa3b52a98665b6be9a8fae2b58894ef1005
Felix Zedén Yverås <felix@fzy.se>
2024-12-03 10:53:34 +0100

    feat(2024/03): solve part 1
    I think this can be done more generic by setting up a generic
    "parse" interface method but I want to see what part 2 brings
    before making such a refactor.

--- solutions/2024/day03/typescript/src/index.ts
+++ solutions/2024/day03/typescript/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { readFileSync } from "fs";
+
+function main() {
+  console.log("Part 1");
+  const memory = readFileSync(__dirname + "/input.txt").toString("utf-8");
+
+  let sum = Array.from(memory.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g)).reduce(
+    (sum, match) => sum + parseInt(match[1]) * parseInt(match[2]),
+    0
+  );
+  console.log(sum);
+}
+
+main();
