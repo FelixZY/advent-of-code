2cdffd03a58834e142d5c247188879fedb8d6361
Felix Zedén Yverås <felix@fzy.se>
2024-12-03 10:58:15 +0100

    test(2024/03): add test to verify part 1 answer
    Also refactor index.ts to be testable.

--- solutions/2024/day03/typescript/src/index.ts
+++ solutions/2024/day03/typescript/src/index.ts
@@ -2,13 +2,16 @@ import { readFileSync } from "fs";
 
 function main() {
   console.log("Part 1");
+  console.log(part1());
+}
+
+export function part1(): number {
   const memory = readFileSync(__dirname + "/input.txt").toString("utf-8");
 
-  let sum = Array.from(memory.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g)).reduce(
+  return Array.from(memory.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g)).reduce(
     (sum, match) => sum + parseInt(match[1]) * parseInt(match[2]),
     0
   );
-  console.log(sum);
 }
 
 main();
---
+++ solutions/2024/day03/typescript/src/part1.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from "bun:test";
+import { part1 } from ".";
+
+test("part1", () => {
+  expect(part1()).toEqual(159892596);
+});
