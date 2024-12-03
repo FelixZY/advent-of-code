bd1b0d72c41003eaa13b391f691d71d826eedc23
Felix Zedén Yverås <felix@fzy.se>
2024-12-03 11:13:14 +0100

    feat(2024/03): complete part 2
    This is a really ugly solution - it works but does not scale well.
    It would be better to split each command - including its parser -
    into its own reusable unit and walk the string character by
    character.

--- solutions/2024/day03/typescript/src/index.ts
+++ solutions/2024/day03/typescript/src/index.ts
@@ -3,6 +3,8 @@ import { readFileSync } from "fs";
 function main() {
   console.log("Part 1");
   console.log(part1());
+  console.log("Part 2");
+  console.log(part2());
 }
 
 export function part1(): number {
@@ -12,6 +14,28 @@ export function part1(): number {
     (sum, match) => sum + parseInt(match[1]) * parseInt(match[2]),
     0
   );
+}
+
+export function part2(): number {
+  const memory = readFileSync(__dirname + "/input.txt").toString("utf-8");
+  let mulEnabled = true;
+
+  return Array.from(
+    memory.matchAll(/(mul)\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don't)\(\)/g)
+  ).reduce((sum, match) => {
+    if (match[1]) {
+      return mulEnabled ? sum + parseInt(match[2]) * parseInt(match[3]) : sum;
+    }
+    if (match[4]) {
+      mulEnabled = true;
+      return sum;
+    }
+    if (match[5]) {
+      mulEnabled = false;
+      return sum;
+    }
+    throw new Error(`Not implemented: ${match}`);
+  }, 0);
 }
 
 main();
