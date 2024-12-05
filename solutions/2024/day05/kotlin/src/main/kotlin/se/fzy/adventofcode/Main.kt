929c434b9f212782c2a952b68c891312afc31ede
Felix Zedén Yverås <felix@fzy.se>
2024-12-05 11:00:07 +0100

    feat(2024/05): complete part 1
    It's interesting switching between kotlin and rust. I find Kotlin to be
    more productive as it better matches with my own thinking and intuition.
    However, rust tends to be a bit more fun with its focus on performance
    and tricky borrowing system. Also, since my work on PrimEvil, connecting
    rust code over the JNI, it has become clear to me just how much overhead
    there is in the JVM.
    
    My solution for part 1 is rather straight-forward and written in the
    main function. I would have liked to optimize the reading a bit so that
    I didn't read the file twice. I think my set-based solution is a fun
    throwback to the university courses on discrete mathematics. However,
    lists do tend to perform better, at least for small datasets, in
    practice. I'm sure I would have solved this in a more efficient manner
    in rust, just because my mindset would be more on performance and
    because the language encourages a more explicit approach.

--- solutions/2024/day05/kotlin/src/main/kotlin/se/fzy/adventofcode/Main.kt
+++ solutions/2024/day05/kotlin/src/main/kotlin/se/fzy/adventofcode/Main.kt
@@ -1,5 +1,35 @@
 package se.fzy.adventofcode
 
 fun main() {
-    println("Hello World!")
+    val input = Resources.readInput("input.txt")
+
+    val ruleSet: Map<Int, Set<Int>> =
+        input.lineSequence().takeWhile { it.isNotBlank() }.takeWhile { it.isNotBlank() }
+            .map { line -> line.split("|").let { it[0].toInt() to it[1].toInt() } }
+            .fold(mutableMapOf<Int, MutableSet<Int>>()) { map, (dependsOn, key) ->
+                map.apply {
+                    getOrPut(key) { mutableSetOf() }.add(dependsOn)
+
+                }
+            }
+    val jobs: List<List<Int>> =
+        input.lineSequence().dropWhile { it.isNotBlank() }.drop(1).takeWhile { it.isNotBlank() }
+            .map { it.split(',').map(String::toInt) }.toList()
+
+    val sumOfMiddle = jobs.filter { job ->
+        val remainingPageSet = job.toMutableSet()
+        val completed = mutableSetOf<Int>()
+        job.forEach { page ->
+            remainingPageSet.remove(page)
+            if (ruleSet[page]?.let {
+                    (it - completed).intersect(remainingPageSet).isEmpty()
+                } == false) {
+                return@filter false
+            }
+            completed.add(page)
+        }
+        true
+    }.sumOf { it[it.size / 2] }
+
+    println(sumOfMiddle)
 }
