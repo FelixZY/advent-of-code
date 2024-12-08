80093638388a1521cb53744708737ea783cf7c2c
Felix Zedén Yverås <felix@fzy.se>
2024-12-08 17:06:34 +0100

    feat(2024/08): implement solution for part 1
    The problem description made things sound more complicated than they
    actually were. I enjoyed this first part.

--- solutions/2024/day08/kotlin/src/main/kotlin/se/fzy/adventofcode/Main.kt
+++ solutions/2024/day08/kotlin/src/main/kotlin/se/fzy/adventofcode/Main.kt
@@ -1,5 +1,48 @@
 package se.fzy.adventofcode
 
 fun main() {
-    println("Hello World!")
+    val input = Resources.readInput("input.txt").trim()
+    val mapBounds =
+        IntVector2d(
+            x = input.lineSequence().take(1).single().length,
+            y = input.lineSequence().count(),
+        )
+    val antennasByFrequency = parseAntennaMap(input)
+
+    val antiNodeCount =
+        antennasByFrequency
+            .mapValues { (_, antennas) -> calculateAntiNodes(antennas) }
+            .values
+            .asSequence()
+            .flatten()
+            .distinct()
+            .filter { it.x >= 0 && it.y >= 0 && it.x < mapBounds.x && it.y < mapBounds.y }
+            .count()
+
+    println(antiNodeCount)
+}
+
+fun calculateAntiNodes(antennas: List<IntVector2d>): Sequence<IntVector2d> = sequence {
+    antennas.indices.forEach { i ->
+        for (j in i + 1..antennas.lastIndex) {
+            val betweenVector = antennas[j] - antennas[i]
+
+            yield(antennas[i] - betweenVector)
+            yield(antennas[j] + betweenVector)
+        }
+    }
+}
+
+fun parseAntennaMap(antennaMap: String): Map<Char, List<IntVector2d>> {
+    val antennasByFrequency = mutableMapOf<Char, MutableList<IntVector2d>>()
+
+    antennaMap.lineSequence().forEachIndexed { y, line ->
+        line.forEachIndexed { x, char ->
+            if (char != '.') {
+                antennasByFrequency.getOrPut(char, ::mutableListOf).add(IntVector2d(x, y))
+            }
+        }
+    }
+
+    return antennasByFrequency
 }
