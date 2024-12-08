73a250fa8df0187f4b52923dca7d9b196ef0ec18
Felix Zedén Yverås <felix@fzy.se>
2024-12-08 16:23:06 +0100

    chore(2024/07): complete part 2
    Given the size of the answers, I'm very happy that I started this
    challenge in python (not having to deal with bigints).
    
    It's quite obvious a major part of the challenge was to handle explosive
    permutation counts in an efficient way. Again, python is pretty good at
    this since it emphasizes the use of iterators over lists. Still,
    performance-wise, I would have preferred using rust or even kotlin.
    Calculations without debug logging takes ~30s for part 2. This is a bit
    long IMO and could be mitigated using multiprocessing.

--- solutions/2024/day07/python/src/main.py
+++ solutions/2024/day07/python/src/main.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from functools import reduce
 from itertools import repeat, chain
 from pathlib import Path
 
@@ -28,6 +27,17 @@ class TimesOperator:
         return hash(self.symbol)
 
 
+@dataclass
+class ConcatOperator:
+    symbol = "||"
+
+    def apply(self, x: int, y: int):
+        return int(str(x) + str(y))
+
+    def __hash__(self):
+        return hash(self.symbol)
+
+
 def main():
     data = [
         [int(item.rstrip(":")) for item in line.split(" ")]
@@ -36,44 +46,45 @@ def main():
         .strip()
         .splitlines()
     ]
-
     calibration_result = 0
 
     for expected, *terms in data:
         operator_count = len(terms) - 1
 
         for operator_sequence in (
-                p
-                for i in range(0, operator_count + 1)
-                for p in distinct_permutations(
-                    chain(
-                        repeat(PlusOperator(), i),
-                        repeat(TimesOperator(), operator_count - i),
-                    )
+            p
+            for pluses in range(0, operator_count + 1)
+            for times in range(operator_count - pluses, -1, -1)
+            for p in distinct_permutations(
+                chain(
+                    repeat(PlusOperator(), pluses),
+                    repeat(TimesOperator(), times),
+                    repeat(ConcatOperator(), operator_count - pluses - times),
                 )
+            )
         ):
             # Perform calculation
             result = terms[0]
-            for (i, operator) in enumerate(operator_sequence):
+            for i, operator in enumerate(operator_sequence):
                 result = operator.apply(result, terms[i + 1])
 
                 if result > expected:
                     break
 
             # Debug log calculation
-            print(
-                reduce(
-                    lambda acc, curr: (
-                        str(curr[1])
-                        if curr[0] == 0
-                        else acc + operator_sequence[curr[0] - 1].symbol + str(curr[1])
-                    ),
-                    enumerate(terms),
-                    "",
-                ),
-                end="=",
-            )
-            print(result)
+            # print(
+            #     reduce(
+            #         lambda acc, curr: (
+            #             str(curr[1])
+            #             if curr[0] == 0
+            #             else acc + operator_sequence[curr[0] - 1].symbol + str(curr[1])
+            #         ),
+            #         enumerate(terms),
+            #         "",
+            #     ),
+            #     end="=",
+            # )
+            # print(result)
 
             if result == expected:
                 calibration_result += result
