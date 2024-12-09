8fa438655193a2da9e0e15d3527371a0acdb6a09
Felix Zedén Yverås <felix@fzy.se>
2024-12-09 21:12:45 +0100

    feat(2024/09): solve part 1
    This is a rather object-oriented approach, which is good as we are
    trying to model a "real" thing. This in turn means the defragmentation
    and checksum functions can be implemented cleanly and separate from each
    other.
    
    A drawback with the object-oriented approach is that we are using more
    memory than needed. In fact, it should be possible to work directly on
    the input string with just a few helper integers rather than a full
    additional list. Such a solution would be able to yield the correct
    sequence of file IDs "stored" on the disk, allowing the checksum to be
    calculated with minimal memory overhead.

--- solutions/2024/day09/rust/src/main.rs
+++ solutions/2024/day09/rust/src/main.rs
@@ -1,3 +1,54 @@
+use std::iter::repeat_n;
+
 fn main() {
-    println!("Hello, world!");
+    let mut disk = Disk::from_disk_map(include_str!("input.txt"));
+    disk.defragment_naive();
+    println!("Checksum: {}", disk.checksum())
+}
+
+struct Disk {
+    blocks: Vec<Option<usize>>,
+}
+
+impl Disk {
+    fn from_disk_map(disk_map: &str) -> Self {
+        Self {
+            blocks: disk_map
+                .trim()
+                .chars()
+                .map(|c| c.to_digit(10).unwrap() as usize)
+                .enumerate()
+                .map(|(i, blocks)| repeat_n(if i % 2 == 0 { Some(i / 2) } else { None }, blocks))
+                .flatten()
+                .collect(),
+        }
+    }
+
+    fn defragment_naive(&mut self) {
+        let mut left = 0;
+        let mut right = self.blocks.len() - 1;
+
+        while left < right {
+            while self.blocks[left].is_some() {
+                left += 1;
+            }
+            while self.blocks[right].is_none() {
+                right -= 1;
+            }
+
+            if left < right {
+                self.blocks[left] = self.blocks[right];
+                self.blocks[right] = None;
+            }
+        }
+    }
+
+    fn checksum(&self) -> u64 {
+        self.blocks
+            .iter()
+            .enumerate()
+            .map(|(i, block)| block.map(|file_id| (file_id * i) as u64))
+            .flatten()
+            .sum()
+    }
 }
